ba19fd03f65cd2a1895393bf44fd2ccdb550aa28
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2021-07-08 11:56:16 +0530

    Update four.cpp

--- array/level-2/four.cpp
+++ array/level-2/four.cpp
@@ -32,5 +32,22 @@ int main() {
     }
   }
   //cout<<count/2<<endl;
+
+  cout<<endl;
+  int numRay[] = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+    int arr_size = sizeof(numRay) / sizeof(numRay[0]);
+   
+   
+    // count the frequency
+    for (int i = 0; i < arr_size; i++) {
+        numRay[numRay[i] % arr_size]
+            = numRay[numRay[i] % arr_size] + arr_size;
+    }
+    cout << "The repeating elements are : " << endl;
+    for (int i = 0; i < arr_size; i++) {
+        if (numRay[i] >= arr_size * 2) {
+            cout << i << " " << endl;
+        }
+    }
   return 0;
 }
