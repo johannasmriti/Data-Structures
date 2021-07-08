e0ebbc6ec3e185ac65582a423d170b398e755bdc
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2021-07-08 11:13:36 +0530

    Update three.cpp

--- array/level-2/three.cpp
+++ array/level-2/three.cpp
@@ -23,19 +23,29 @@ int main() {
   int n;
   cin>>n;
   int a[n];
+  map<int,int> mp;
   for(int i=0;i<n;i++){
     cin>>a[i];
+    mp[a[i]]++;
   }
   int sum;
   cin>>sum; 
   int count=0;
   for(int i=0;i<n;i++){
+    count+=mp[sum-a[i]];
+
+    if(sum-a[i]==a[i]){
+      count--;
+    }
+  }
+  /*for(int i=0;i<n;i++){
     for(int j=i+1;j<n;j++){
       if(a[i]+a[j]==sum){
         count++;
       }
     }
-  }
-  cout<<count<<endl;
+  }*/
+  
+  cout<<count/2<<endl;
   return 0;
 }
