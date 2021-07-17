6680a978dd7050ff042848f072f5dba689c5d3e0
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2021-07-17 07:52:11 +0530

    Update ten.cpp

--- array/level-2/ten.cpp
+++ array/level-2/ten.cpp
@@ -1,66 +1,3 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-void swap(int *a,int *b){
-  int t=*a;
-  *a=*b;
-  *b=t;
-}
-
-void rearrange(int a[],int n){
-  int x,temp,j;
-  for(int i=0;i<n;i++){
-    if(a[i]>0){
-      //x=i;
-      j=i;
-      //x=a[j];
-      while(a[i]>0 ){
-        i++;
-      }
-      //swap(&a[i],&a[x]);
-      if(i<n){
-        x=j;
-        temp=a[i];
-        a[i]=a[j];
-        a[j]=temp;
-        i--;
-      }
-    }
-  }
-  int pivot=x+1;
-  int d=0;
-  for(int i=0;i<n;i++){
-    if(d<pivot){
-      if(i%2==0){
-        cout<<a[pivot]<<" ";
-        pivot++;
-      }
-      else{
-        cout<<a[d]<<" ";
-        d++;
-      }
-    }
-    else{
-      cout<<a[pivot]<<" ";
-      pivot++;
-    }
-  }
-}
-
-int main(){
-  int n;
-  cin>>n;
-  int a[n];
-  for(int i=0;i<n;i++){
-    cin>>a[i];
-  }
-  rearrange(a,n);
-  /*for(int i=0;i<n;i++){
-    cout<<a[i]<<" ";
-  }*/
-  return 0;
-}
 /*Rearrange array in alternating positive & negative items with O(1) extra space | Set 1
 Difficulty Level : Hard
 Last Updated : 29 May, 2021
@@ -74,3 +11,31 @@ Output: arr[] = {-4, 1, -1, 2, 3, 4}
 
 Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
 output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0}*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  int j=0,x;
+  for(int i=0;i<n;i++){
+    if(a[i]<0){
+      x=a[i];
+      for(int k=i;k>=j;k--){
+        a[k]=a[k-1];
+      }
+      a[j]=x;
+      j+=2;
+    }
+  }
+  for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+  }
+  return 0;
+}
