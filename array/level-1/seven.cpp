9dce75175c9006ead1ab5a16c97704645ccff2cc
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2021-07-22 23:47:33 +0530

    Update seven.cpp

--- array/level-1/seven.cpp
+++ array/level-1/seven.cpp
@@ -9,7 +9,19 @@ Input: {0, 1, 2, 0, 1, 2}
 Output: {0, 0, 1, 1, 2, 2}
 
 Input: {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
-Output: {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}*/
+Output: {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+maps
+2--3
+1--4   a--b
+1.2--a
+9--5
+sorts the elements
+space comp=O(1)
+all the ele - put it in the map
+
+0-3
+1-2
+2-2*/
 
 #include <bits/stdc++.h>
 
@@ -18,13 +30,19 @@ using namespace std;
 int main() {
   int n;
   cin>>n;
+ //defining the map
+ //element-frequency
   map<int,int> mp;
-
+//1,0,2,1,0,2
   for(int i=0;i<n;i++){
     int c;
     cin>>c;
+   //0--2
+   //1--2
+   //2--2
     mp[c]++;
-  } 
+   
+  } //index i=0
   for(auto i:mp){
     for(int j=0;j<i.second;j++){
       cout<<i.first<<" ";
